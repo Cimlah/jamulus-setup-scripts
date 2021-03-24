59c0bb3485a0ed00f2a1cf322120948dd78de47d
Cimlah <28655852+Cimlah@users.noreply.github.com>
2021-03-24 07:45:42 +0000

    Changed script working, so no matter if these files exist, script goes on

--- into-jamulus-server/jamulus-update.sh
+++ into-jamulus-server/jamulus-update.sh
@@ -3,7 +3,7 @@
 cd /jamulus-server/ && \
 source /jamulus-server/jamulus-stop.sh
 if [[ $pgrep_exit_code == "0" ]]; then
-    rm -rf jamulus-latest latest.tar.gz && \
+    rm -rf jamulus-latest latest.tar.gz ; \
     wget https://github.com/jamulussoftware/jamulus/archive/latest.tar.gz && \
     tar -xvf latest.tar.gz && \
     cd jamulus-latest/ && \
@@ -12,7 +12,7 @@ if [[ $pgrep_exit_code == "0" ]]; then
     rm /usr/local/bin/Jamulus ; \
     cp Jamulus /usr/local/bin/
 elif [[ $pgrep_exit_code == "2" ]]; then
-    rm -rf jamulus-latest latest.tar.gz && \
+    rm -rf jamulus-latest latest.tar.gz ; \
     wget https://github.com/jamulussoftware/jamulus/archive/latest.tar.gz && \
     tar -xvf latest.tar.gz && \
     cd jamulus-latest/ && \
